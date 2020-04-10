ad37500a8e780cf0febd443762231429a68c4493
Yuliia Engman <yuliia.engman@gmail.com>
2020-04-10 14:31:39 -0400

    testing updated code for deposit detail VC

--- KVO-Lab/DepositDetailViewController.swift
+++ KVO-Lab/DepositDetailViewController.swift
@@ -12,15 +12,40 @@ class DepositDetailViewController: UIViewController {
     
     @IBOutlet weak var amountTextField: UITextField!
     
+    // I need observation here as well to know from what balance to substract or add
+    private var balanceObservation: NSKeyValueObservation?
+    
+    // I need pass user here too - that we will reffer to the same user
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //configureUserBalanceObservation()
     }
     
+//    private func configureUserBalanceObservation() {
+//        balanceObservation = User.shared.observe(\.balance, options: [.old], changeHandler: { [weak self](settings, change) in
+//            let userBalance = change.oldValue
+//        })
+//    }
+    
     @IBAction func depositButtonPressed(_ sender: UIButton) {
+        // + addint to the balance
+        balanceObservation = User.shared.observe(\.balance, options: [.old], changeHandler: { [weak self](settings, change) in
+            guard let oldUserBalance = change.oldValue else { return }
+            
+            guard let deposit = self?.amountTextField.text, !deposit.isEmpty else {
+            print("missing fields")
+            return
+            }
+            
+            User.shared.balance = oldUserBalance + Double(deposit)!
+            
+            self?.amountTextField.resignFirstResponder()
+    })
     }
     
     @IBAction func withdrawButtonPressed(_ sender: UIButton) {
+        // - substracting from the balance
     }
     
 }
