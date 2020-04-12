270ba9af7312d1208e9baa96963ca0a65db8456a
Yuliia Engman <yuliia.engman@gmail.com>
2020-04-12 18:09:02 -0400

    code is not appending to array but substituting users

--- KVO-Lab/DepositWithdrawalViewController.swift
+++ KVO-Lab/DepositWithdrawalViewController.swift
@@ -10,16 +10,16 @@ import UIKit
 
 class DepositWithdrawalViewController: UIViewController {
     
-    // is it array of Users or Accounts?
-        private var arrayOfUsers = [User]() {
-            didSet {
-                tableView.reloadData()
-            }
-        }
-    
     @IBOutlet weak var tableView: UITableView!
     
     private var arrayOfUsersObservation: NSKeyValueObservation?
+    
+    // is it array of Users or Accounts?
+    private var arrayOfUsers =  Accounts.shared.arrayOfUsers {
+        didSet {
+            tableView.reloadData()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +27,7 @@ class DepositWithdrawalViewController: UIViewController {
         configureTableView()
         //tableView.reloadData()
         loadData()
+        configureUserArrayObservation()
     }
     
     private func configureTableView() {
@@ -38,24 +39,28 @@ class DepositWithdrawalViewController: UIViewController {
         arrayOfUsers = Accounts.shared.arrayOfUsers
     }
     
+    private func configureUserArrayObservation() {
+        arrayOfUsersObservation = Accounts.shared.observe(\.arrayOfUsers, options: [.new], changeHandler: { [weak self](settings, change) in
+            guard let usersArray = change.newValue else { return }
+            self?.arrayOfUsers = usersArray
+        })
+    }
 }
 
 extension DepositWithdrawalViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       
-        //return Accounts.shared.arrayOfUsers.count
         return arrayOfUsers.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "userInfoCell", for: indexPath)
         
-        arrayOfUsersObservation = Accounts.shared.observe(\.arrayOfUsers, options: [.new], changeHandler: { (settings, change) in
-            let userAccount = change.newValue![indexPath.row]
-            cell.textLabel?.text = userAccount.name
-            cell.detailTextLabel?.text = String(userAccount.balance)
-        })
+        //        arrayOfUsersObservation = Accounts.shared.observe(\.arrayOfUsers, options: [.new], changeHandler: { (settings, change) in
+        //let userAccount = change.newValue![indexPath.row]
+        let user = arrayOfUsers[indexPath.row]
+        cell.textLabel?.text = user.name
+        cell.detailTextLabel?.text = String(user.balance)
         return cell
     }
 }
