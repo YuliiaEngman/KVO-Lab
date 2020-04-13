baed3ad698a0be1d84a4a301c1d4c3eaddc054e8
Yuliia Engman <yuliia.engman@gmail.com>
2020-04-12 20:50:05 -0400

    updated code for user view controller

--- KVO-Lab/UserInfoViewController.swift
+++ KVO-Lab/UserInfoViewController.swift
@@ -10,37 +10,56 @@ import UIKit
 
 class UserInfoViewController: UIViewController {
     
-    //    private var users = [User]() {
-    //        didSet {
-    //            tableView.reloadData()
-    //        }
-    //    }
-    
     @IBOutlet weak var tableView: UITableView!
+    
+    private var arrayOfUsersObservation: NSKeyValueObservation?
+    
+    // is it array of Users or Accounts?
+    private var arrayOfUsers =  Accounts.shared.arrayOfUsers {
+        didSet {
+            tableView.reloadData()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         configureTableView()
-
+        //tableView.reloadData()
+        loadData()
+        configureUserArrayObservation()
     }
     
-
     private func configureTableView() {
         tableView.dataSource = self
+    }
+    
+    private func loadData() {
+        arrayOfUsers = Accounts.shared.arrayOfUsers
+    }
+    
+    private func configureUserArrayObservation() {
+        arrayOfUsersObservation = Accounts.shared.observe(\.arrayOfUsers, options: [.new], changeHandler: { [weak self](settings, change) in
+            guard let usersArray = change.newValue else { return }
+            self?.arrayOfUsers = usersArray
+        })
     }
 
 }
 
 extension UserInfoViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "userInfoCell", for: indexPath)
-        //let user = users[indexPath.row]
-        cell.textLabel?.text = "Name"
-        cell.detailTextLabel?.text = "Amount"
-        return cell
-    }
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+          return arrayOfUsers.count
+      }
+      
+      func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+          let cell = tableView.dequeueReusableCell(withIdentifier: "userInfoCell", for: indexPath)
+          
+          //        arrayOfUsersObservation = Accounts.shared.observe(\.arrayOfUsers, options: [.new], changeHandler: { (settings, change) in
+          //let userAccount = change.newValue![indexPath.row]
+          let user = arrayOfUsers[indexPath.row]
+          cell.textLabel?.text = user.name
+          cell.detailTextLabel?.text = String(user.balance)
+          return cell
+      }
 }
